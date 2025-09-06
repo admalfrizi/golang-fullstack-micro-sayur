aad6a27e5b0ba3d7b7844b5b6276cbb3db46dcb3
adamalfarizi <adam.alfarizi.2002@gmail.com>
2025-09-06 12:30:39 +0700

    fixing for misuse in migration with {} and ()

--- user-service/database/migrations/000001_create_users_table.up.sql
+++ user-service/database/migrations/000001_create_users_table.up.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS users {
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    photo varchar(255) NULL,
     phone varchar(17) NULL,
+    photo varchar(255) NULL,
     address TEXT NULL,
     lat VARCHAR(50) NULL,
     lng VARCHAR(50) NULL,
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS users {
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NULL,
     deleted_at TIMESTAMP NULL   
-}
+);
 
 CREATE INDEX idx_users_email ON users(email);
 CREATE INDEX idx_users_is_verified ON users(is_verified);
